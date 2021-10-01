93b6b2ae30345484bca6d526f24bd856c6e0a8ad
Moonline <moonline@users.noreply.github.com>
2021-10-01 18:40:55 -0300

    Enhance menu

--- src/index.js
+++ src/index.js
@@ -10,53 +10,72 @@ function createWindow() {
 		width: 800,
 		height: 600
 	});
-
+	let menu;
 	let currentFile;
 
-	const menu = new Menu();
-	menu.append(new MenuItem({
-		id: 'ipen',
-		label: 'Open',
-		toolTip: 'Open worklog file',
-		click() {
-			dialog.showOpenDialog({ properties: ['openFile'] })
-				.then(result => {
-					console.log(result.canceled);
-					console.log(result.filePaths);
-					if (result.filePaths.length > 0 && result.filePaths[0]) {
-						currentFile = result.filePaths[0];
-						mainWindow.loadURL('data:text/html;charset=utf-8,' + encodeURI(main(currentFile)));
+	const openFile = () => {
+		dialog.showOpenDialog({ properties: ['openFile'] })
+			.then(result => {
+				console.log(result.canceled);
+				console.log(result.filePaths);
+				if (result.filePaths.length > 0 && result.filePaths[0]) {
+					currentFile = result.filePaths[0];
+					mainWindow.loadURL('data:text/html;charset=utf-8,' + encodeURI(main(currentFile)));
 
-						// TODO
-						menu.getMenuItemById('reload').disabled = false;
+					// TODO
+					// menu.getMenuItemById('reload').disabled = false;
+				}
+			}).catch(error => {
+				console.error(error);
+			});
+	};
+
+	menu = Menu.buildFromTemplate([
+		{
+			label: 'File',
+			submenu: [
+				{
+					id: 'ipen',
+					label: 'Open worklog',
+					accelerator: 'CommandOrControl+O',
+					click: openFile
+				}, {
+					id: 'reload',
+					label: 'Reload',
+					disabled: true,
+					accelerator: 'CommandOrControl+R',
+					click() {
+						if (currentFile) {
+							mainWindow.loadURL('data:text/html;charset=utf-8,' + encodeURI(main(currentFile)));
+						}
 					}
-				}).catch(error => {
-					console.error(error);
-				});
+				},
+				{ role: 'quit' }
+			]
+		}, {
+			label: 'Edit',
+			submenu: [
+				{
+					id: 'openWorklogFile',
+					label: 'Edit worklog',
+					disabled: true,
+					accelerator: 'CommandOrControl+E',
+					click: () => {
+						currentFile && Open(currentFile);
+					}
+				},
+				{
+					id: 'openWorklogDirectory',
+					label: 'Open containing directory',
+					disabled: true,
+					accelerator: 'CommandOrControl+D',
+					click: () => {
+						currentFile && Open(Path.dirname(currentFile));
+					}
+				}
+			]
 		}
-	}));
-	menu.append(new MenuItem({
-		id: 'reload',
-		label: 'Reload',
-		disabled: true,
-		toolTip: 'reload worklog file',
-		click() {
-			if (currentFile) {
-				mainWindow.loadURL('data:text/html;charset=utf-8,' + encodeURI(main(currentFile)));
-			}
-		}
-	}));
-	menu.append(new MenuItem({
-		id: 'edit',
-		label: 'Edit worklog',
-		disabled: true,
-		toolTip: 'Edit worklog file in system editor',
-		click() {
-			if (currentFile) {
-				Open(currentFile);
-			}
-		}
-	}));
+	]);
 	Menu.setApplicationMenu(menu);
 }
 
